fa0386f9c2bb3695d805f13c072b77e0f5a6a7f9
mikeylbc <41922542+mikeylbc@users.noreply.github.com>
2024-08-12 14:47:14 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -7,7 +7,7 @@ export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VI
 
 /*   STEP 2   */
 export const homepageText = `
-  <p>Welcome Beer League</p>  
+  <p>Welcome Beer League</p>
 `;
 
 /*   STEP 3   */
